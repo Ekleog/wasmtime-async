3129d6eab4f477ea99101a4af7d959e05e15af25
Léo Gaspard <leo@gaspard.io>
2020-05-02 22:51:57 +0200

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,35 @@
+let
+  hostPkgs = import <nixpkgs> {};
+  pkgsSrc = hostPkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    # The following is for nixos-unstable as of 2020-04-04
+    rev = "ae6bdcc53584aaf20211ce1814bea97ece08a248";
+    sha256 = "0hjhznns1cxgl3hww2d5si6vhy36pnm53hms9h338v6r633dcy77";
+  };
+  rustOverlaySrc = hostPkgs.fetchFromGitHub {
+    owner = "mozilla";
+    repo = "nixpkgs-mozilla";
+    # The following is the latest version as of 2020-04-04
+    rev = "e912ed483e980dfb4666ae0ed17845c4220e5e7c";
+    sha256 = "08fvzb8w80bkkabc1iyhzd15f4sm7ra10jn32kfch5klgl0gj3j3";
+  };
+  rustOverlay = import rustOverlaySrc;
+  pkgs = import pkgsSrc {
+    overlays = [ rustOverlay ];
+  };
+  rustNightlyChannel = pkgs.rustChannelOf {
+    date = "2020-03-12";
+    channel = "nightly";
+    sha256 = "0gjnl37hqalcw0c69chnc2r6n40a0w8w2bvwrmp3bz183wawp6fh";
+  };
+  rustStableChannel = pkgs.rustChannelOf {
+    date = "2020-03-12";
+    channel = "stable";
+    sha256 = "0pddwpkpwnihw37r8s92wamls8v0mgya67g9m8h6p5zwgh4il1z6";
+  };
+in
+pkgs.stdenv.mkDerivation {
+  name = "rlua-async";
+  buildInputs = (with rustStableChannel; [ rust rustfmt-preview ]);
+}
